f43511e148e3afe5f59fdadb7eb9b4ddaac83eb9
luanm <mll2071@rit.edu>
2025-03-20 13:00:42 -0400

    rename collection

--- src/main/java/com/booksly/app/User.java
+++ src/main/java/com/booksly/app/User.java
@@ -386,9 +386,9 @@ public class User {
         PreparedStatement ps = CONNECTION.prepareStatement(
                 "update collection set name = ? where user_id = ? and collection_id = ?");
 
-        ps.setString(1, name);
+        ps.setInt(1, collectionId);
         ps.setInt(2, this.userId);
-        ps.setInt(3, collectionId);
+        ps.setString(3, name);
 
         ps.executeUpdate();
     }
